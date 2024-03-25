dcbd25467f750ce1468aa92f81152a7bd4fccd97
Martin <martincanedagz@gmail.com>
2024-03-25 15:52:09 +0100

    testing us2 and us3 done, 2 and 2, 9 total

---
+++ src/components/Spotlight/Spotlight.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import Spotlight from "./Spotlight";
+
+const artworks = [
+  {
+    slug: 14,
+    name: "La Rendición de Breda",
+    artist: "Diego Velázquez",
+    imageSource:
+      "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+  },
+];
+
+beforeEach(() => {
+  render(<Spotlight pieces={artworks} />);
+});
+
+test("renders an image", () => {
+  const image = screen.getByRole("img");
+  expect(image).toBeInTheDocument();
+});
+test("renders an artist info", () => {
+  const artistInfo = screen.getByText(/Artist: Diego Velázquez/i);
+  expect(artistInfo).toBeInTheDocument();
+});
